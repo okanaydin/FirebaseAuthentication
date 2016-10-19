94379a9873f9f0877724aeb0a53590883140060a
Okan AYDIN <okanaydin1994@gmail.com>
2016-10-19 19:27:34 +0300

    Activity oluşturuldu

---
+++ app/src/main/java/com/example/okanaydin/firebaseauthentication/UyeOl.java
@@ -0,0 +1,17 @@
+package com.example.okanaydin.firebaseauthentication;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class UyeOl extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_uye_ol);
+
+
+
+    }
+}
